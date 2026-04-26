87043b34cc76fa7f6d6086d6b48d29a9259f65ed
YehuditRayman <y0556741787@gmail.com>
2026-04-26 15:28:43 +0300

    Update Firebase config to use env vars and fix CORS order

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,9 +1,16 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('../../firebase-service-account.json');
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  // התיקון הזה קריטי: הוא מוודא שירידות השורה במפתח הפרטי נקראות נכון
+  privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+};
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+}
 
 module.exports = { firebaseAdmin: admin };
